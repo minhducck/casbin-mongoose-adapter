2fbccf0a0afa4e78ed8da49c97d0f34463c400a1
Ta Minh Duc <minhducck@kreebox>
2021-06-14 18:57:43 +0800

    Change type to module
    Update Deps
    
    Update tsConfig
    
    Update Deps
    
    Update Deps
    
    Update

---
+++ index.ts
@@ -0,0 +1,4 @@
+export * from './src/index'
+export * from './src/interface/casbin-rule.interface'
+export * from './src/interface/policy-storage.interface'
+export * from './src/schema/casbin-rule.schema'
